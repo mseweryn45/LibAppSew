aed8af1d83e9d6303b2277d96b9562372bfb599d
Michał Seweryn <mseweryn@>
2021-10-16 12:20:47 +0200

    Added Book model and corresponding with Random view

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibAppSew.Models;
+
+namespace LibAppSew.Controllers
+{
+    public class BooksController : Controller
+    {
+        //będzie pokazywała randomową książkę 
+
+        public IActionResult Random()
+        {
+            var firstBook = new Book() { Name = "English dictionary" };
+            return View(firstBook);
+        }
+    }
+}
