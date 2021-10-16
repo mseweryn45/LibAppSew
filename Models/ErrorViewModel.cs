58d0321244f0f16ddb323fc04f3e6aee3ad3eec4
Michał Seweryn <mseweryn@>
2021-10-16 11:52:06 +0200

    Init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LibAppSew.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
